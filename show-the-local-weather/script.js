32d645e3617eb0bedd4906013605e09ef5f51b6c
Knut Jackowski <doxanthropos@googlemail.com>
2017-01-03 22:15:52 +0100

    got some weather for the weather app.

--- show-the-local-weather/script.js
+++ show-the-local-weather/script.js
@@ -1,25 +1,34 @@
 window.onload = function(){
+  var latitude;// = 50.314382099999996;
+  var longitude;// =7.5623594;
 
-/*
-First we need to find the users location
-*/
-if ("geolocation" in navigator) {
-  console.log("geolocation is available");
-} else {
-  console.log("geolocation IS NOT available");
-}
+  /*
+  First we need to find the users location
+  */
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position){
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      console.log("Latitude : " + latitude);
+      console.log("Longitude : " + longitude);
+      /* my key for OpenWeatherMap: 420a256bd565195fc4cfe5da7c01cf51 */
+      var url = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=420a256bd565195fc4cfe5da7c01cf51";
+      $.getJSON(url,function(weather){
+        console.log(weather)
+      });
+    });
+  } else {
+    console.log("geolocation IS NOT available");
+  }
 
-navigator.geolocation.getCurrentPosition(function(position){
-  console.log("Latitude  : " + position.coords.latitude);
-  console.log("Longitude : " + position.coords.longitude);
-});
 
-/*
-Secondly based on the location, we ask OpenWeatherMap for the weather in this location.
-*/
 
-/*
-And lastly we display the weather on the page
-*/
+  /*
+  Secondly based on the location, we ask OpenWeatherMap for the weather in this location.
+  */
+
+  /*
+  And lastly we display the weather on the page
+  */
 
 }
