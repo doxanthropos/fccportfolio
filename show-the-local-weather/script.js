0ace13d10ad6b06a3ce72d630a426b8729e5de01
Knut Jackowski <doxanthropos@googlemail.com>
2017-01-05 14:42:39 +0100

    Switchin between Fahrenheit and Celsius works

--- show-the-local-weather/script.js
+++ show-the-local-weather/script.js
@@ -1,3 +1,4 @@
+/* The first part is responsible for the actual weather */
 window.onload = function(){
   var latitude;// = 50.314382099999996;
   var longitude;// =7.5623594;
@@ -9,12 +10,14 @@ window.onload = function(){
     navigator.geolocation.getCurrentPosition(function(position){
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
+      /* Secondly, we get the weather data for the location */
       /* my key for OpenWeatherMap: 420a256bd565195fc4cfe5da7c01cf51 */
       var key = "420a256bd565195fc4cfe5da7c01cf51";
       var url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&APPID=420a256bd565195fc4cfe5da7c01cf51";
       $.getJSON(url,function(weather){
         var city = weather.name;
         console.log(city);
+        /* Finally we display the data on the website */
         document.getElementById("location").innerHTML = city + ", " + weather.sys.country;
         document.getElementById("temperatureC").innerHTML = (Math.round(weather.main.temp-273.15));
         /* converting to Fahrenheit according to http://www.rapidtables.com/convert/temperature/how-kelvin-to-fahrenheit.htm */
@@ -27,14 +30,15 @@ window.onload = function(){
     console.log("geolocation IS NOT available");
   }
 
+/* From here on you will find the UI */
 
+/* switching between Fahrenheit and Celsius */
 
-  /*
-  Secondly based on the location, we ask OpenWeatherMap for the weather in this location.
-  */
-
-  /*
-  And lastly we display the weather on the page
-  */
-
-}
+$("#celsius").click(function(){
+  this.style.display = "none";
+  document.getElementById("fahrenheit").style.display = "block";
+});
+$("#fahrenheit").click(function(){
+  this.style.display = "none";
+  document.getElementById("celsius").style.display = "block";
+});
