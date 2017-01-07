1d2a1926907ebf572e1bfa55fefdb8f04c2f3779
Knut Jackowski <doxanthropos@googlemail.com>
2017-01-07 20:43:02 +0100

    added input field for search

--- build-a-wikipedia-viewer/script.js
+++ build-a-wikipedia-viewer/script.js
@@ -1,5 +1,11 @@
+function getInput(){
+  document.getElementById("output").innerHTML = document.getElementById("searchInput").value;
+}
 $(document).ready(function(){
 
+
+
+/* build the list of articles */
 var data = {results:[{title:"Title 1",url:"example.com"},{title:"Title 2",url:"example.com"}]}
 
 var theTemplateScript = $("#resultList").html();
